2be36165fa5ea72b835a6fa8f3cbe50e9e23209b
Andrew Latimer <andrew@elpasoera.com>
2010-12-21 12:42:20 -0700

    initial commit

--- lib/four_ruby.rb
+++ lib/four_ruby.rb
@@ -1,7 +1,7 @@
 directory = File.expand_path(File.dirname(__FILE__))
 
 require File.join directory, 'four_ruby', 'base'
-require File.join directory, 'four_ruby', 'oauth'
+require File.join directory, 'four_ruby', 'oauth2'
 
 module FourRuby
 end
